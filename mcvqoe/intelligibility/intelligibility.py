2479757720585798279c370c972b89ae247fbedb
Jesse <jesse.frey@nist.gov>
2021-07-28 07:57:37 -0600

    Remove terminal_progress_update (now in mcvqoe)

--- mcvqoe/intelligibility/intelligibility.py
+++ mcvqoe/intelligibility/intelligibility.py
@@ -12,26 +12,10 @@ import sys
 import os.path
 import datetime
 
+
+from mcvqoe.base.terminal_user import terminal_progress_update
 #version import for logging purposes
 from .version import version
-     
-def terminal_progress_update(prog_type,num_trials,current_trial,err_msg=""):
-    if(prog_type=='proc'):
-        if(current_trial==0):
-            #we are post processing
-            print('Processing test data')        
-        if(current_trial % 10 == 0):
-            print(f'Processing trial {current_trial+1} of {num_trials}')
-    elif(prog_type=='test'):
-        if(current_trial==0):
-            print(f'Starting Test of {num_trials} trials')
-        if(current_trial % 10 == 0):
-            print(f'-----Trial {current_trial} of {num_trials}')
-    elif(prog_type=='check-fail'):
-        print(f'On trial {current_trial+1} of {num_trials} : {err_msg}')
-        
-    #continue test
-    return True
 
 def chans_to_string(chans):
     #check if we got a string
